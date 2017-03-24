f48de8f54124e80684ec878e2f6daa8f80640e7d
Kamil Ronewicz <galileox86@gmail.com>
2017-03-24 15:41:45 +0100

    Remove not needed code

--- src/login/index.js
+++ src/login/index.js
@@ -1,25 +1,5 @@
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-
 module.exports = {
   verifyUser(accessToken, refreshToken, profile, done) {
     done(null, profile)
-  },
-  init(CONFIG, application){
-    passport.use(new GoogleStrategy({
-      clientID: CONFIG.GOOGLE_OAUTH_CLIENT_ID,
-      clientSecret: CONFIG.GOOGLE_OAUTH_SECRET_ID,
-      callbackURL: `http://localhost:${CONFIG.APP_LISTEN_PORT}/auth/google/callback`
-    }, this.verifyUser))
-
-    passport.serializeUser(function (user, done) {
-      done(null, user)
-    })
-
-    passport.deserializeUser(function (user, done) {
-      done(null, user)
-    })
-
-    application.use(passport.initialize())
   }
 }
